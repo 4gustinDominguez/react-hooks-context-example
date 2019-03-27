9d9e5979ee7b131f141c635dd66866e210df006b
Nicholas Bordeau <nrb@NRB-MBP.local>
2019-03-27 10:58:32 -0400

    Removed excess files and added useState().

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = props => {
+  const [count = 0, setCount] = useState(props.count);
+  const [text = '', setText] = useState();
+
+  return (
+    <div>
+      <p>
+        The current {text || 'count'} is {count}.
+      </p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <p>
+        <input
+          type="text"
+          value={text}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+        />
+      </p>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
