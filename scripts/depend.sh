96728a67df31b4177b094c68dffc9737f40462f6
Nick Bordeau <nbordeau@pipusa.com>
2019-07-03 09:17:41 -0400

    Clear /var/www old project before adding new

--- scripts/depend.sh
+++ scripts/depend.sh
@@ -8,5 +8,8 @@ sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm install
 # Build Production Environment
 sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build
 
+# Remove all old project files
+sudo rm -rf /var/www/*
+
 # Move production files to /var/www/
 sudo mv /home/ec2-user/react-hooks/build/* /var/www/
