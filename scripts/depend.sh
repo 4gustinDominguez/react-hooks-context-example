71feabf681f932020baf8421401c27513595c132
Nick Bordeau <nbordeau@pipusa.com>
2019-07-03 08:35:23 -0400

    Added AWS CodeDeploy appspec and script to build project

---
+++ scripts/depend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/ec2-user/react-hooks
+
+# Install dependencies
+sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm install
+
+# Build Production Environment
+sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build
