108dcf1170922db79d98b87336df79983e2285ae
Nick Bordeau <nbordeau@pipusa.com>
2019-07-03 09:35:14 -0400

    Added a test comment to test deployment.

--- scripts/depend.sh
+++ scripts/depend.sh
@@ -11,5 +11,7 @@ sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build
 # Remove all old project files
 sudo rm -rf /var/www/*
 
+#Test Comment
+
 # Move production files to /var/www/
 sudo mv /home/ec2-user/react-hooks/build/* /var/www/
