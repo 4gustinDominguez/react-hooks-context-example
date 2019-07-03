3f409335594b8a519b339f2dc4c8fb94e0d68c9f
Nick Bordeau <nbordeau@pipusa.com>
2019-07-03 09:11:21 -0400

    Moved all files to /var/www/ when complete

--- scripts/depend.sh
+++ scripts/depend.sh
@@ -7,3 +7,6 @@ sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm install
 
 # Build Production Environment
 sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build
+
+# Move production files to /var/www/
+sudo mv ~/react-hooks/build/* ~/var/www/
