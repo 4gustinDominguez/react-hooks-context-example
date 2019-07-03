1c5aa5a971aade30da2e3af5ac139b59495d3b85
Nick Bordeau <nbordeau@pipusa.com>
2019-07-03 09:13:49 -0400

    Updated syntax

--- scripts/depend.sh
+++ scripts/depend.sh
@@ -9,4 +9,4 @@ sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm install
 sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build
 
 # Move production files to /var/www/
-sudo mv ~/react-hooks/build/* ~/var/www/
+sudo mv /home/ec2-user/react-hooks/build/* /var/www/
